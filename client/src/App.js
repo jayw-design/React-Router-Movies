0f6e89a25f791eed59289b0481eb5552dc6092d0
heliokles <heliokles@github.com>
2021-04-07 21:41:06 -0700

    initial-upload

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,15 @@ export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
+
+
   useEffect(() => {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
         .then(response => {
           // Study this response with a breakpoint or log statements
+
           // and set the response data as the 'movieList' slice of state
         })
         .catch(error => {
@@ -24,6 +27,7 @@ export default function App () {
 
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
+    
   };
 
   return (
